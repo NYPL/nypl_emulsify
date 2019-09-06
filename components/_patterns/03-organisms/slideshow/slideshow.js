b5bc3961326fbb1d65e0b5d5e246ab36e733d057
Brian Lewis <brian@modulesunraveled.com>
2019-09-06 10:03:33 -0500

    [107] Added support for the image title.

--- components/_patterns/03-organisms/slideshow/slideshow.js
+++ components/_patterns/03-organisms/slideshow/slideshow.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
   $('.slideshow').each(function() {
     var currentViewer = $('.slideshow__viewer', this)
+    var currentTitle = $('.slideshow__image-title', this)
     var currentCarousel = $('.slideshow__carousel', this)
     // The Viewer at the top
     currentViewer.not('.slick-initialized').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: false,
-      asNavFor: '.slideshow__carousel'
+      asNavFor: '.slideshow__carousel, .slideshow__image-title'
+    });
+    currentTitle.not('.slick-initialized').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false
     });
     // The Carousel at the bottom
     currentCarousel.not('.slick-initialized').slick({
@@ -16,7 +22,7 @@ $(document).ready(function(){
       infinite: true,
       accessibility: true,
       mobileFirst: true,
-      asNavFor: '.slideshow__viewer',
+      asNavFor: '.slideshow__viewer, .slideshow__image-title',
       prevArrow: $('.slideshow__nav--prev', this),
       nextArrow: $('.slideshow__nav--next', this),
       centerMode: true,
